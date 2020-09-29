d36d90a462935033a534cbcc775e97ef95d38b84
psine1 <71101870+psine1@users.noreply.github.com>
2020-09-29 19:07:55 -0300

    Add files via upload

---
+++ works/autozone/Ads/300x600/banner.js
@@ -0,0 +1,191 @@
+FORGE.init = function() {
+  var width = FORGE.utils.getAdSizeFromMeta().width;
+  var height = FORGE.utils.getAdSizeFromMeta().height;
+  var banner = FORGE.createElement({id: "forge-banner", backgroundColor: "#333", width: width, height: height, cursor: "pointer", overflow: "hidden", parent: document.body});
+  var ctaActive = false;
+
+  var images = ["CTA.png", "BoshLogo.png", "txt1-1.png", "txt1-2.png", "txt1-3.png", "txt1-4.png",
+    "Fr1-bg.jpg", "Fr2-bg.jpg", "Fr6-bg.jpg", "Wiper.png", "txt4.png", "AutoZone-Logo.png", "ElementBar.png"
+  ];
+
+  FORGE.preloadImages(images, true, function() {
+
+    /* Create elements
+    ================================================= */
+    var Fr1 = FORGE.createElement({backgroundImage: "Fr1-bg.jpg", width: 300, height: 600, parent: banner});
+    var txt1 = FORGE.createElement({ width: 300, height: 600, parent: banner});
+
+    var txt1_1 = FORGE.createElement({backgroundImage: "txt1-1.png", width: 300, height: 600, parent: txt1});
+    var txt1_2 = FORGE.createElement({backgroundImage: "txt1-2.png", width: 300, height: 600, parent: txt1});
+    var txt1_3 = FORGE.createElement({backgroundImage: "txt1-3.png", width: 300, height: 600, parent: txt1});
+    var txt1_4 = FORGE.createElement({backgroundImage: "txt1-4.png", width: 300, height: 600, parent: txt1});
+
+    var Fr2 = FORGE.imageCache["Fr2-bg.jpg"];
+
+    var canvas = FORGE.createElement({type: "canvas", width: 300, height: 600, parent: banner});
+
+    var Wiper = FORGE.createElement({backgroundImage: "Wiper.png", width: 300, height: 650, left: 0, top: -5, parent: banner});
+
+
+
+
+    var Fr6 = FORGE.createElement({backgroundImage: "Fr6-bg.jpg", width: 300, height: 600, parent: banner});
+    var txt4 = FORGE.createElement({backgroundImage: "txt4.png", width: 300, height: 600, parent: Fr6});
+    var AutoZoneLogo = FORGE.createElement({backgroundImage: "AutoZone-Logo.png", width: 300, height: 600, parent: Fr6});
+    var elementBar = FORGE.createElement({backgroundImage: "ElementBar.png", width: 300, height: 600, parent: Fr6});
+
+
+    var contCTA = FORGE.createElement({width: 300, height: 600, parent: banner});
+    var CTA = FORGE.createElement({backgroundImage: "CTA.png", left: 172, top: 403, width: 101, height: 31, retina: true, parent: contCTA});
+    var BoshLogo = FORGE.createElement({backgroundImage: "BoshLogo.png", width: 300, height: 600, parent: contCTA});
+
+   // var txt3 = FORGE.imageCache["txt3.png"];
+
+   // var txt3 = FORGE.createElement({backgroundImage: "txt3.png", width: 300, height: 600, parent: banner});
+
+    /* Create additional elements
+    ============================================ */
+   // var imageWipeMask = createImageWipeMask();
+    var border = FORGE.createElement({border: "solid 1px #000", boxSizing: "border-box", width: width, height: height, parent: banner});
+
+    /* Animations
+    ============================================ */
+
+    Wiper.set({transformOrigin:'150px 645px 0px'});
+
+
+
+
+    function slideText(obj) {
+      return new FORGE.Timeline()
+      .from(obj, 1, {opacity: 0, x: 30, ease: Power3.easeOut});
+    }
+
+
+
+
+
+    canvas.in = new FORGE.Timeline()
+      .from(canvas, 0.3, {opacity: 0, ease: Power3.easeOut, delay: 0.2})
+    ;
+
+    Wiper.in = new FORGE.Timeline()
+      .fromTo(Wiper, 3.5, {rotation: -100, x: 0, ease: Power0.easeNone}, {rotation: 105, x: 0})
+    ;
+
+
+    function fadeBg(obj) {
+      return new FORGE.Timeline()
+        .from(obj, 1, {opacity: 0, ease: Power3.easeOut})
+    }
+
+
+    Fr6.in = new FORGE.Timeline()
+      .from(Fr6, 1, {x: -300, ease: Power3.easeOut})
+    ;
+
+   /* elementBar.in = new FORGE.Timeline()
+      .to(elementBar, 1, {y: -600, ease: Power3.easeOut})
+    ;*/
+
+    contCTA.in = new FORGE.Timeline()
+      .from(contCTA, 1, {x: 300, ease: Power3.easeOut})
+    ;    
+
+    /* Timeline
+    ============================================ */
+    var main_tl = new FORGE.Timeline()
+      .offset(0.25)
+
+      .add(slideText(txt1_1))
+      .offset(0.2)
+      .add(slideText(txt1_2))
+      .offset(0.2)
+      .add(slideText(txt1_3))
+      .offset(0.2)
+      .add(slideText(txt1_4))
+      .offset(2)
+
+      .add(createImageWipeMask)
+      .add(canvas.in)
+      .offset(0.02)
+
+      .add(Wiper.in)
+      .offset(4)
+
+      .add(Fr6.in)
+      .offset(0.5)
+      .add(contCTA.in)
+
+     /* .add(elementBar.in)
+      .offset(0)*/
+
+
+    /* Helpers
+    ============================================ */
+    function createImageWipeMask() {
+      var ctx = canvas.getContext("2d");
+      var cx = 150;
+      var cy = 600;
+      var radius = 450;
+
+      canvas.startAngle = 1 * Math.PI;
+      canvas.endAngle = 1.5 * Math.PI;
+      
+      canvas.start = function() {
+        canvas.renderInterval = setInterval(canvas.render, 30);
+      };
+
+      canvas.stop = function() {
+        clearInterval(canvas.renderInterval);
+      };
+
+      canvas.render = function() {
+        ctx.clearRect(0, 0, width, height);
+        ctx.beginPath();
+        ctx.moveTo(cx,cy);
+        ctx.arc(cx, cy, radius, canvas.startAngle, canvas.endAngle);
+        ctx.closePath();
+        ctx.fill();
+
+        ctx.globalCompositeOperation = "source-in";
+
+        ctx.drawImage(Fr2, 0, 0, width, height);
+
+        ctx.globalCompositeOperation = "source-over";
+      }
+
+      canvas.render();
+
+
+
+
+      canvas.foil = new FORGE.Timeline()
+        .add(function() { canvas.start(); })
+        .chain()
+        .from(canvas, 1, {endAngle: 1 * Math.PI, ease: Power0.easeNone})
+        .chain()
+        .to(canvas, 1, {endAngle: 2.1 * Math.PI, ease: Power0.easeNone})
+        .chain()
+        .add(function() { canvas.stop(); });
+
+      return canvas;
+    }
+
+
+
+
+    /* Interactions
+    ============================================ */
+    banner.onmouseenter = function() {
+          TweenMax.to(CTA, 0.3, {scale: 1.02, force3D: false, rotation: 0.01, ease: Power3.easeOut})
+    }
+
+    banner.onmouseleave = function() {
+          TweenMax.to(CTA, 0.3, {scale: 1, force3D: false, rotation: 0.01, ease: Power3.easeOut})
+    }
+
+
+
+  });
+};
---
+++ works/autozone/Ads/320x50/banner.js
@@ -0,0 +1,221 @@
+FORGE.init = function() {
+  var width = FORGE.utils.getAdSizeFromMeta().width;
+  var height = FORGE.utils.getAdSizeFromMeta().height;
+  var banner = FORGE.createElement({id: "forge-banner", backgroundColor: "#333", width: width, height: height, cursor: "pointer", overflow: "hidden", parent: document.body});
+  var ctaActive = false;
+
+  var images = [
+  "CTA.png",
+  "BoshLogo.png", 
+  "Fr1-bg.jpg",
+  "Fr2-bg.jpg",
+  "txt1.png",
+  "txt2.png",
+  "txt3.png",
+  "Wiper.png",
+  "AutoZone-Logo.png",
+  "ElementBar.png"
+  ];
+
+  FORGE.preloadImages(images, true, function() {
+
+    /* Create elements
+    ================================================= */
+    var Fr1 = FORGE.createElement({backgroundImage: "Fr1-bg.jpg", width: 320, height: 50, parent: banner});
+    var txt_1 = FORGE.createElement({backgroundImage: "txt1.png", width: 320, height: 50, parent: banner});
+
+    var Fr2 = FORGE.createElement({width: 320, height: 50, parent: banner});
+    var Fr2_bg = FORGE.createElement({backgroundImage: "Fr2-bg.jpg", width: 320, height: 50, parent: Fr2});
+
+    var txt_2 = FORGE.createElement({width: 320, height: 50, parent: banner});
+    var txt_2_bg = FORGE.createElement({backgroundImage: "txt2.png", width: 320, height: 50, parent: txt_2});
+
+
+    var elementBar = FORGE.createElement({width: 320, height: 50, parent: banner});
+    var elementBar_bg = FORGE.createElement({backgroundImage: "ElementBar.png", parent: elementBar});
+
+
+
+    var Wiper = FORGE.createElement({backgroundImage: "Wiper.png", width: 320, height: 50, left: 0, top: 0, parent: banner});
+
+
+    var txt_3 = FORGE.createElement({backgroundImage: "txt3.png", width: 320, height: 50, parent: banner});
+
+    var AutoZoneLogo = FORGE.createElement({backgroundImage: "AutoZone-Logo.png", width: 320, height: 50, parent: banner});
+
+    var CTA = FORGE.createElement({backgroundImage: "CTA.png", left: 125, top: 25, width: 82, height: 16, retina: true, parent: banner});
+    var BoshLogo = FORGE.createElement({backgroundImage: "BoshLogo.png", width: 320, height: 50, parent: banner});
+
+
+    /* Create additional elements
+    ============================================ */
+   // var imageWipeMask = createImageWipeMask();
+    var border = FORGE.createElement({border: "solid 1px #000", boxSizing: "border-box", width: width, height: height, parent: banner});
+
+    /* Animations
+    ============================================ */
+
+    Wiper.set({transformOrigin:'right bottom'});
+
+
+    function fadeInLeft(obj) {
+      return new FORGE.Timeline()
+      .from(obj, 1, {opacity: 0, x: 15, ease: Power3.easeOut});
+    }
+
+
+    function slideOutLeft(obj) {
+      return new FORGE.Timeline()
+      .to(obj, 1.5, { x: -320, ease: Power3.easeOut});
+    }
+
+
+    function fadeOut(obj) {
+      return new FORGE.Timeline()
+      .to(obj, 1, {opacity: 0, ease: Power3.easeOut});
+    }
+
+
+
+
+
+
+    function fadein(obj) {
+      return new FORGE.Timeline()
+        .from(obj, 1, {opacity: 0, ease: Power3.easeOut})
+    }
+
+
+    function slideInUp(obj) {
+      return new FORGE.Timeline()
+      .from(obj, 1, { y: 50, ease: Power3.easeOut});
+    }
+
+
+    function slideInLeft(obj) {
+      return new FORGE.Timeline()
+      .from(obj, 1.5, { x: 320, ease: Power3.easeOut});
+    }
+
+
+    Wiper.in = new FORGE.Timeline()
+      .fromTo(Wiper, 1.5, {x: -320, ease: Power2.easeInOut}, { x: 20})
+    ;
+    var rectMaskIn = function(obj) {
+      return new FORGE.Timeline()
+        .fromTo(obj, 1.5, {clip: 'clip:"rect(0px 0px 50px 0px)', ease: Power2.easeIn}, {clip: 'clip:"rect(0px 320px 320px 0px)'});
+    }
+
+
+
+
+
+    elementBar.in = new FORGE.Timeline()
+      .add(rectMaskIn(elementBar))
+      .from(elementBar_bg, 3, {left: 0, ease: Sine.easeOut})
+
+    Fr2.in = new FORGE.Timeline()
+      .add(rectMaskIn(Fr2))
+      .from(Fr2_bg, 3, {left: 0, ease: Sine.easeOut})
+
+    txt_2.in = new FORGE.Timeline()
+      .add(rectMaskIn(txt_2))
+      .from(txt_2_bg, 3, {left: 0, ease: Sine.easeOut})
+
+
+    /* Timeline
+    ============================================ */
+    var main_tl = new FORGE.Timeline()
+      .offset(0.25)
+
+
+      .add(fadeInLeft(txt_1))
+      .offset(2)
+
+      .add(Wiper.in)
+      .add(elementBar.in)
+      .add(Fr2.in)
+      .add(txt_2.in)
+
+      .offset(4.5)
+      .add(fadeOut(txt_2))
+      .add(slideOutLeft(elementBar))
+      .offset(1.5)
+      .add(fadein(txt_3))
+      .add(slideInLeft(BoshLogo))
+      .offset(0.5)
+      .add(slideInUp(AutoZoneLogo))
+      .add(slideInUp(CTA))
+
+
+
+    /* Helpers
+    ============================================ */
+  
+/*
+    function createImageWipeMask() {
+      var ctx = canvas.getContext("2d");
+      var cx = width;
+      var cy = 320;
+      var radius = 305;
+
+      canvas.startAngle = 1 * Math.PI;
+      canvas.endAngle = 1.5 * Math.PI;
+      
+      canvas.start = function() {
+        canvas.renderInterval = setInterval(canvas.render, 30);
+      };
+
+      canvas.stop = function() {
+        clearInterval(canvas.renderInterval);
+      };
+
+      canvas.render = function() {
+        ctx.clearRect(0, 0, width, height);
+        ctx.beginPath();
+        ctx.moveTo(cx,cy);
+        ctx.arc(cx, cy, radius, canvas.startAngle, canvas.endAngle);
+        ctx.closePath();
+        ctx.fill();
+
+        ctx.globalCompositeOperation = "source-in";
+
+        ctx.drawImage(Fr2, 0, 0, width, height);
+
+        ctx.globalCompositeOperation = "source-over";
+      }
+
+      canvas.render();
+
+
+
+
+      canvas.foil = new FORGE.Timeline()
+        .add(function() { canvas.start(); })
+        .chain()
+        .from(canvas, 1.5, {endAngle: 1 * Math.PI, ease: Power2.easeInOut})
+        .chain()
+        .to(canvas, 1.5, {endAngle: 2 * Math.PI})
+        .chain()
+        .add(function() { canvas.stop(); });
+
+      return canvas;
+    }
+
+*/
+
+
+    /* Interactions
+    ============================================ */
+    banner.onmouseenter = function() {
+          TweenMax.to(CTA, 0.3, {scale: 1.02, force3D: false, rotation: 0.01, ease: Power3.easeOut})
+    }
+
+    banner.onmouseleave = function() {
+          TweenMax.to(CTA, 0.3, {scale: 1, force3D: false, rotation: 0.01, ease: Power3.easeOut})
+    }
+
+
+
+  });
+};
